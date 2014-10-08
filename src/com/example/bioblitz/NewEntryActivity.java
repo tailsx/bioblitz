41aba22a756f88f4291928d729bd6c6c0e52336d
tailsx <davidof.chen@mail.utoronto.ca>
2014-10-07 23:23:43 -0400

    Added buttons to new entry. Also added new data activity. In new entry, added a save and cancel button.  Also have a way to close keyboard after typing. Also added new data activity to switch to after clicking on save or cancel button.

--- src/com/example/bioblitz/NewEntryActivity.java
+++ src/com/example/bioblitz/NewEntryActivity.java
@@ -1,10 +1,14 @@
 package com.example.bioblitz;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class NewEntryActivity extends Activity {
@@ -23,6 +27,19 @@ public class NewEntryActivity extends Activity {
 		imageView.setImageBitmap(image);
 	}
 
+	
+	public void saveRecord(View view) {
+		EditText commonName = (EditText) findViewById(R.id.commonNameField);
+		Intent intent = new Intent(this, DataActivity.class);
+		intent.putExtra("common", commonName.getText().toString());
+		startActivity(intent);
+	}
+	
+	public void cancel(View view) {
+		Intent intent = new Intent(this, DataActivity.class);
+		startActivity(intent);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
